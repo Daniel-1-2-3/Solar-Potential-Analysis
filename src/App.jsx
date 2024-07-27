9822fa227d1095b2910baa91f4c5c86f4f1a7c5c
Daniel-1-2-3 <danielzhu501@gmail.com>
2024-07-27 12:39:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className='w-full bg-indigo-100 flex items-center justify-center min-h-screen'>
+      <div className='w-3/4 bg-indigo-200 flex flex-col items-center justify-center min-h-screen text-center'>  
+        <p className='text-4xl text-black'>Placeholder</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
